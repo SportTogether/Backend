780016f125f3c2a2046a0af1287efbcdf92c4764
ThanhBear <leethanh24003@gmail.com>
2023-09-27 16:42:39 +0700

    update user controller

---
+++ src/main/java/com/sportTogether/SportTogether/SportTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.sportTogether.SportTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTogetherApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sportTogether/SportTogether/controller/userController.java
@@ -0,0 +1,4 @@
+package com.sportTogether.SportTogether.controller;
+
+public class userController {
+}
