5cd3166092e1d9332e9ded411cfac944ae389f71
ThanhBear <leethanh24003@gmail.com>
2023-09-27 20:16:48 +0700

    update folders

--- src/main/java/com/sportTogether/SportTogether/controller/userController.java
+++ src/main/java/com/sportTogether/SportTogether/controller/userController.java
@@ -1,4 +1,20 @@
 package com.sportTogether.SportTogether.controller;
 
+import com.sportTogether.SportTogether.payload.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class userController {
+    @GetMapping
+    public ResponseEntity<?> getAllUsers ()
+    {
+        Response response = new Response( 200,"OKOK","hello world");
+
+        return new ResponseEntity<>(response, HttpStatus.OK) ;
+    }
 }
---
+++ src/main/java/com/sportTogether/SportTogether/payload/Response.java
@@ -0,0 +1,40 @@
+package com.sportTogether.SportTogether.payload;
+
+public class Response {
+    private int  statusCode ;
+    private String message ;
+    private Object data ;
+
+    public Response() {
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Response(int statusCode, String message, Object data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+}
