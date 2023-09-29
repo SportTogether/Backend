d3eb7e26cce92fe6928384d90d68f9bd2d0ef919
ThanhBear <leethanh24003@gmail.com>
2023-09-29 13:46:08 +0700

    test public api

--- src/main/java/com/sportTogether/SportTogether/controller/userController.java
+++ src/main/java/com/sportTogether/SportTogether/controller/userController.java
@@ -1,20 +1,27 @@
 package com.sportTogether.SportTogether.controller;
 
-import com.sportTogether.SportTogether.payload.Response;
-import org.springframework.http.HttpStatus;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
-public class userController {
-    @GetMapping
-    public ResponseEntity<?> getAllUsers ()
-    {
-        Response response = new Response( 200,"OKOK","hello world");
+public class userController implements RequestHandler<String ,String > {
+//    @GetMapping
+//    public ResponseEntity<?> getAllUsers ()
+//    {
+//        Response response = new Response( 200,"OKOK","hello world");
+//
+//        return new ResponseEntity<>(response, HttpStatus.OK) ;
+//        }
 
-        return new ResponseEntity<>(response, HttpStatus.OK) ;
+
+    @Override
+    public String handleRequest(String s, Context context) {
+        return "hello world";
     }
+
+
 }
